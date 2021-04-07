4842eb78a94d4e2f3aa03e3ecac6fbc299314348
Kaveesha <gnis0001@student.monash.edu>
2021-04-07 20:29:58 +0530

    Login Page and dashboard UI
    Complete Login UI
    Navigation drawer for the Dashboard is complete
    Naming appbar needs to change to the user name
    A list view for the cards need to be added

--- app/src/main/java/com/example/studenttutormatchapp/MainActivity.java
+++ app/src/main/java/com/example/studenttutormatchapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.studenttutormatchapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_dashboard_layout);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle navToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_string,R.string.close_string);
+
+        drawerLayout.addDrawerListener(navToggle);
+        navToggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
     }
 }
