58cbddd0c37e535aacafc1c4ca60a3f5bd64e973
Sadeeptha Bandara <hban0006@student.monash.edu>
2021-04-22 00:32:34 +0530

    Modifying gradle to use API key, change API request to use Authorization header

--- app/src/main/java/com/example/studenttutormatchapp/remote/APIUtils.java
+++ app/src/main/java/com/example/studenttutormatchapp/remote/APIUtils.java
@@ -1,12 +1,16 @@
 package com.example.studenttutormatchapp.remote;
 
+import com.example.studenttutormatchapp.BuildConfig;
+
 public class APIUtils {
+
 
     private APIUtils(){
 
     };
+    String API_KEY = BuildConfig.FIT3077_API_KEY;
 
-    public static final String API_URL = "";
+    public static final String API_URL = "https://fit3077.com/api/v1";
 
     public static UserService getUserService(){
         return RetrofitClient.getClient(API_URL).create(UserService.class);
--- app/src/main/java/com/example/studenttutormatchapp/remote/RetrofitClient.java
+++ app/src/main/java/com/example/studenttutormatchapp/remote/RetrofitClient.java
@@ -1,5 +1,13 @@
 package com.example.studenttutormatchapp.remote;
 
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,6 +16,18 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(String url){
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                Request request = chain.request()
+                        .newBuilder()
+//                        .addHeader("app_id", APP_ID)
+//                        .addHeader("app_key", APP_KEY)
+                        .build();
+                return chain.proceed(request);
+            }
+        }).build();
+
         if (retrofit == null){
             retrofit = new Retrofit.Builder().baseUrl(url)
                     .addConverterFactory(GsonConverterFactory.create()).build();
