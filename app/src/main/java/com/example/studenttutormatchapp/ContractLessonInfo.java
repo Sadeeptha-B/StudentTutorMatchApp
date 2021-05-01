4a52255458d16bd0ce2365a311c5de1f975eeaab
Kaveesha <gnis0001@student.monash.edu>
2021-05-01 19:14:06 +0530

    Basic contract classes

---
+++ app/src/main/java/com/example/studenttutormatchapp/ContractLessonInfo.java
@@ -0,0 +1,14 @@
+package com.example.studenttutormatchapp;
+
+public class ContractLessonInfo {
+
+    String lessonDay;
+    String lessonTime;
+
+
+    public ContractLessonInfo(String lessonDay, String lessonTime){
+        this.lessonDay = lessonDay;
+        this.lessonTime = lessonTime;
+    }
+
+}
